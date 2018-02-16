38821456d691d3ceb2680034a54efbae7b6f52b3
shiva-z <sa.zamani73@yahoo.com>
2018-02-16 11:14:43 -0700

    added intra cluster density

--- src/get_net_property.py
+++ src/get_net_property.py
@@ -3,6 +3,39 @@ import argparse
 import networkx as nx
 import json
 
+
+def intra_cluster_dens(connections, comsize):
+    '''
+    return a dictionary in which keys are clusters and values are intra cluster density (#internal edges of C / (n_c*(n_c-1)/2))
+    '''
+    intra_dens = {}
+    for com in comsize:
+        if com in connections and com in connections[com]:
+            intra_dens[com] = connections[com][com] / (comsize[com] * (comsize[com]-1))
+        else:
+            intra_dens[com] = 0
+    return intra_dens
+
+def get_connections(G,communities):
+    connections = {}
+    comsize = {}
+    for node in G:
+        com = communities[node]
+        if com not in comsize:
+            comsize[com] = 1
+        else:
+            comsize[com] += 1    
+        if com not in connections:
+            connections[com] = {}
+        for neigh in G.neighbors(node):
+            neighcom = communities[neigh]
+            if neighcom not in connections[com]:
+                connections[com][neighcom] = 1.0
+            else:
+                connections[com][neighcom] += 1.0 
+                
+    return connections, comsize
+        
 def get_degree_dist(G):
     degrees = {}
     for node in G.nodes():
@@ -52,7 +85,9 @@ def __main():
         if File.endswith(".community"):
                 continue
         with open("properties/"+File, 'w') as output:
-            G, communities = loadNet(args.nets+"/"+File)    
+            G, communities = loadNet(args.nets+"/"+File)
+            connections, comsize = get_connections(G,communities)   
+            intra_dens = intra_cluster_dens(connections, comsize)
             output.write("nodes "+str(len(G.nodes()))+"\n")
             output.write("edges "+str(len(G.edges()))+"\n")
             output.write("communities "+str(len(set(communities.values())))+"\n")
@@ -64,6 +99,9 @@ def __main():
             output.write("degree_assortativity "+str(nx.degree_assortativity_coefficient(G))+"\n")
             output.write("average_shortest_path "+str(nx.average_shortest_path_length(G))+"\n")
             output.write("diameter "+str(nx.diameter(G))+"\n")
+            output.write("intra_cluster_density ")
+            output.write(json.dumps(intra_dens))
+            output.write("\n")
 
 if __name__ == "__main__":
     __main()
