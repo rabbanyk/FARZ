a4c8704fcf6e4e6f53e7beea9efa3f5b653a577e
shiva-z <sa.zamani73@yahoo.com>
2018-05-17 11:23:44 -0600

    create latex table presenting properties of nets

--- src/create_table.py
+++ src/create_table.py
@@ -3,7 +3,7 @@ import os
 import argparse
 import networkx as nx
 import json
-
+import decimal
 
 def loadNetStats(path):
     stats={}
@@ -19,24 +19,45 @@ def __main():
     parser.add_argument('net_stats', help='Path to the properties of networks')
     args = parser.parse_args()
     stats={}
-    parameters = ['nodes','edges','communities','diameter','average_shortest_path','degree_assortativity','avg_clustering_coef','mixing_parameter']
+    parameters = ['nodes','edges','communities','diameter','average_shortest_path','degree_assortativity','avg_clustering_coef']
     print("\\begin{table}[]")
     print("\centering")
     print("\caption{My caption}")
     print("\label{my-label}")
-    print("\\begin{tabular}{|c|c|c|c|c|c|c|c|c|}")
+    print("\\begin{tabular}{|c|c|c|c|c|c|c|c|}")
     print("\hline")
-    #print("& nodes & edges & communities & diameter & average\_shortest\_path & degree\_assortativity & avg\_clustering\_coef \\\\ \hline")
+    prop_list = ['nodes', 'edges','communities','diameter', 'average\_shortest\_path', 'degree\_assortativity','avg\_clustering\_coef']
+    for prop in prop_list:
+        print(" & \\rotatebox{90}{ "+prop+" }", end='')
+    print("  \\\\ \hline")
     for File in sorted(os.listdir(args.net_stats)):
         stats[File] = loadNetStats(args.net_stats+"/"+File)
-        print(" & \\rotatebox{90}{ "+File.replace('_','-')+" }", end='')
-        #print(File+"& "+stats["nodes"]+" & "+stats["edges"]+" & "+stats["communities"]+" & "+stats["diameter"]+" & "+stats["average_shortest_path"]+" & "+stats["degree_assortativity"]+" & "+stats["avg_clustering_coef"]+" \\\\ \hline")
-    print("  \\\\ \hline")   
-    for p in parameters:
-        print(p.replace('_',' '), end='')
-        for File in sorted(os.listdir(args.net_stats)):
-            print(" & "+stats[File][p], end='')
-        print("  \\\\ \hline")    
+        print(File.replace('_','-'), end='')
+        for p in parameters:
+            if "avg" not in stats[File]:
+                if isinstance(stats[File][p], float):
+                    print(" & "+str(round(stats[File][p],2)), end='') 
+                else:
+                    print(" & "+str(stats[File][p]), end='') 
+            else:
+                if isinstance(stats[File]["avg"][p], float):
+                    avg = decimal.Decimal(str(round(stats[File]["avg"][p],2))).normalize()
+                    std = decimal.Decimal(str(round(stats[File]["std"][p],2))).normalize()
+                    print(" & "+str(avg)+"("+str(std)+")", end='') 
+                else:
+                    print(" & -", end='')
+        print("  \\\\ \hline")
+    #print("& nodes & edges & communities & diameter & average\_shortest\_path & degree\_assortativity & avg\_clustering\_coef \\\\ \hline")
+    # for File in sorted(os.listdir(args.net_stats)):
+    #     stats[File] = loadNetStats(args.net_stats+"/"+File)
+    #     print(" & \\rotatebox{90}{ "+File.replace('_','-')+" }", end='')
+    #     #print(File+"& "+stats["nodes"]+" & "+stats["edges"]+" & "+stats["communities"]+" & "+stats["diameter"]+" & "+stats["average_shortest_path"]+" & "+stats["degree_assortativity"]+" & "+stats["avg_clustering_coef"]+" \\\\ \hline")
+    # print("  \\\\ \hline")   
+    # for p in parameters:
+    #     print(p.replace('_',' '), end='')
+    #     for File in sorted(os.listdir(args.net_stats)):
+    #         print(" & "+stats[File][p], end='')
+    #     print("  \\\\ \hline")    
     print("\end{tabular}")
     print("\end{table}")    
 if __name__ == "__main__":
